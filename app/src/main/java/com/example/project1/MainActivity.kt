79a029e0aba44a04890857cef24f6e138de9535f
OroszErikOE <riki199925@gmail.com>
2021-11-06 01:59:13 +0100

    shake test

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.project1
 
+import android.hardware.Sensor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.hardware.SensorManager
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private val SHAKE_THRESHOLD = 3.25f // m/S**2
+    private val MIN_TIME_BETWEEN_SHAKES_MILLISECS = 1000
+    private var mLastShakeTime: Long = 0
+    private var mSensorMgr: SensorManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mSensorMgr = getSystemService(SENSOR_SERVICE) as SensorManager?;
+        val accelerometer: Sensor? = mSensorMgr!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        if (accelerometer != null) {
+            mSensorMgr!!.registerListener(
+                this, accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+
+
+
+
     }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val curTime = System.currentTimeMillis()
+            if (curTime - mLastShakeTime > MIN_TIME_BETWEEN_SHAKES_MILLISECS) {
+                val x = event!!.values[0]
+                val y = event!!.values[1]
+                val z = event!!.values[2]
+                val acceleration = Math.sqrt(
+                    Math.pow(x.toDouble(), 2.0) + Math.pow(y.toDouble(), 2.0) + Math.pow(z.toDouble(), 2.0)
+                ) - SensorManager.GRAVITY_EARTH
+                Log.d("APP_NAME", "Acceleration is " + acceleration + "m/s^2")
+                if (acceleration > SHAKE_THRESHOLD) {
+                    mLastShakeTime = curTime
+                    Log.d("APP_NAME", "Shake, Rattle, and Roll")
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+private fun SensorManager.registerListener(mainActivity: MainActivity, accelerometer: Sensor, sensorDelayNormal: Int) {
+
 }

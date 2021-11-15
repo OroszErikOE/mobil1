f6a0bbd6214f7d228663145f7937fa2cd79bf305
OroszErikOE <riki199925@gmail.com>
2021-11-15 21:30:25 +0100

    sound and animation 2.0

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -8,17 +8,10 @@ import android.hardware.SensorManager
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.media.MediaPlayer
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationSet
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
-import android.widget.Toast
 import com.example.project1.databinding.ActivityMainBinding
 import java.util.*
-import kotlin.math.pow
+import kotlin.math.abs
 import kotlin.math.sqrt
 
 
@@ -28,56 +21,46 @@ class MainActivity : AppCompatActivity() {
     private var currentAcceleration = 0f
     private var lastAcceleration = 0f
     private var mediaPlayer : MediaPlayer? = MediaPlayer()
-    private var isplaying : Boolean = false
-    //private var imageView = null findViewById<ImageView>(R.id.image_view)
+    private var isPlaying : Boolean = false
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        Objects.requireNonNull(sensorManager)!!.registerListener(sensorListener, sensorManager!!
+        Objects.requireNonNull(sensorManager)?.registerListener(sensorListener, sensorManager!!
             .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
         acceleration = 1f
         currentAcceleration = SensorManager.GRAVITY_EARTH
         lastAcceleration = SensorManager.GRAVITY_EARTH
 
-        mediaPlayer!!.setLooping(true)
-        mediaPlayer = MediaPlayer.create(this, R.raw.bell_sound)
-
-
-        //val imageView = findViewById<ImageView>(R.id.image_view)
-        //imageView.animation = AnimationUtils.loadAnimation( this,R.anim.shake_animation)
-
-
     }
+
     private val sensorListener: SensorEventListener = object : SensorEventListener {
         override fun onSensorChanged(event: SensorEvent) {
-            //onstopAnimation()
             val x = event.values[0]
             val y = event.values[1]
             val z = event.values[2]
             lastAcceleration = currentAcceleration
             currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
             val delta: Float = currentAcceleration - lastAcceleration
-            acceleration = acceleration * 0.9f + delta
-
-            if (acceleration > 5) {
-                Log.d("Teszt####", acceleration.toString())
-                if (!isplaying) {
-                    mediaPlayer!!.start()
+            acceleration = abs(acceleration * 0.9f + delta)
+            if (acceleration > 1.6) {
+                if (!isPlaying) {
                     onStartAnimation()
-                    Toast.makeText(applicationContext, "Shake event detected", Toast.LENGTH_SHORT).show()
-                    isplaying = true
+                    mediaPlayer = MediaPlayer.create(applicationContext, R.raw.bell_sound)
+                    mediaPlayer!!.isLooping=true
+                    mediaPlayer!!.start()
+                    isPlaying = true
                 }
 
             }
             else{
-                if(isplaying)
+                if(isPlaying)
                 {
-                    isplaying = false
-                    //mediaPlayer!!.stop()
-                    onstopAnimation()
+                    isPlaying = false
+                    mediaPlayer!!.stop()
+                    onsTopAnimation()
                 }
             }
         }
@@ -85,26 +68,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onStartAnimation() {
-
         val animation = AnimationUtils.loadAnimation( this.applicationContext,R.anim.shake_animation )
-        //binding.imageView.
         binding.imageView.startAnimation(animation)
-        //animation = AnimationUtils.loadAnimation( this.applicationContext,R.anim.shake_animation)
-        //binding.imageView.animation.start()
-
-
     }
-    private fun onstopAnimation() {
-        Log.d("Teszt####", "Haliitt vagyok")
-        Thread.sleep(2000)
+    private fun onsTopAnimation() {
+        Thread.sleep(10)
         binding.imageView.clearAnimation()
     }
 
 
 
     override fun onResume() {
-        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
-            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+            sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+           Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
 
         )
         super.onResume()
